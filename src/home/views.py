198fcc62713833c736ffded3abaf19ac7fb4c35d
Yanis Romanovich <thrasherdota2@gmail.com>
2020-01-09 18:01:58 +0300

    Albina

--- src/home/views.py
+++ src/home/views.py
@@ -6,6 +6,7 @@ def index(request):
     return render(request, "index.html", {
         "data":[
             "Skidon project",
-            system()
+            system(),
+            "Albina Kondratenko, i love you"
     ],
     })
