7b11bfb14ada1b8cf8c1479b0b754498903e8b9b
Yanis Romanovich <thrasherdota2@gmail.com>
2020-02-02 20:31:46 +0300

    New app(home) fixed

--- src/about/views.py
+++ src/about/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
 
-from platform import system
-
 
 def about(request):
-    return render(
-        request,
-        "index.html",
-        {
-            "data": [
-                "Skidon project",
-                system(),
-            ]
-        },
-    )
+    return render(request, "index.html")
