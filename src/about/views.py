3794068308500b6558083e1c2efbbda804d542c3
Yanis Romanovich <thrasherdota2@gmail.com>
2020-02-19 21:25:38 +0300

    xxx

--- src/about/views.py
+++ src/about/views.py
@@ -1,10 +1,10 @@
 from django.views.generic import TemplateView
-from about.models import Katalog,Discount
+from about.models import Katalog, Discount
 
 class AboutView(TemplateView):
     http_method_names =('get','post')
 
-    template_name="about/templates/index.html"
+    template_name="about/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("", include("about.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [path("", include("about.urls")),
+               path("admin/", admin.site.urls)]
