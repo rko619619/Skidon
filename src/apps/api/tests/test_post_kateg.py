8f83ebae87f9eec4978097e754a82aee9f5248cb
Yanis Romanovich <thrasherdota2@gmail.com>
2020-03-09 19:43:06 +0300

    fixed post_kateg

--- src/apps/api/tests/test_post_kateg.py
+++ src/apps/api/tests/test_post_kateg.py
@@ -43,13 +43,13 @@ class Post_kategTest(ApiTest):
         user_headers = {"HTTP_AUTHORIZATION": self.user_token}
         admin_headers = {"HTTP_AUTHORIZATION": self.admin_token}
         data = {
-            "name": "name"
+            "name": "name",
                 }
 
-        response = self.client.post("/api/v1/post_kateg/", data=data, **user_headers)
+        response = self.client.post("/api/v1/post_kateg/", data=data,content_type="application/json", **user_headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = self.client.post("/api/v1/post_kateg/", data=data, **admin_headers)
+        response = self.client.post("/api/v1/post_kateg/", data=data,content_type="application/json", **admin_headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_update(self):
