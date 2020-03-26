32c79abe6f9134bbc0d8c979c64822fb3e99c753
Yanis Romanovich <thrasherdota2@gmail.com>
2020-03-26 23:46:29 +0300

    fixed models

--- src/apps/about/models/discount.py
+++ src/apps/about/models/discount.py
@@ -3,8 +3,8 @@ from django.db import models as m
 
 class Discount(m.Model):
     shop = m.TextField(blank=True)
-    name_of_discount = m.TextField(unique=True)
-    text = m.TextField(blank=True)
+    name_of_discount = m.TextField(blank=True)
+    text = m.TextField(blank=True,unique=True)
     price = m.TextField(blank=True)
     additional_media = m.URLField(blank=True)
     media = m.URLField(blank=True)
